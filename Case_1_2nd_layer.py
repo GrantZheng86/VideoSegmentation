914ebf72d201ffc111a14393154e1706b45bcaf0
GrantZheng86 <33077268+GrantZheng86@users.noreply.github.com>
2021-08-26 00:18:59 -0600

    Need to fix extension problem

--- Case_1_2nd_layer.py
+++ Case_1_2nd_layer.py
@@ -7,11 +7,13 @@ LAYER_THICKNESS = 10
 PERCENTILE = 80
 STARTING_HEIGHT = 70
 LARGE_AREA_THRESHOLD = 5000
+DEBUMP_BLOCK_SIZE = 10
 
 
 def extract_contours(img, crop_location):
     if len(img.shape) == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    original_img = img.copy()
 
     bgr_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     img = img[0:crop_location, :]
@@ -26,13 +28,123 @@ def extract_contours(img, crop_location):
 
     top_contour = top_info[1]
     top_contour[:, 1] = top_contour[:, 1] + (im_h - seg_height)
-    remove_bumps(top_contour)
 
-    cv2.polylines(bgr_img, [top_info[1]], False, (0, 255, 0))
-    cv2.imshow('bw', bgr_img)
+    bottom_contour = bottom_info[1]
+    bottom_contour[:, 1] = bottom_contour[:, 1] + (im_h - seg_height)
+    left_bound, right_bound = find_image_boundary(original_img)
+
+    top_contour = top_contour[10:-10, :]
+    bottom_contour = bottom_contour[10:-10, :]
+
+    bottom_contour = extend_contour(bottom_contour, left_bound, right_bound, original_img.shape[1])
+    top_contour = extend_contour(top_contour, left_bound, right_bound, original_img.shape[1])
+
+    cv2.polylines(bgr_img, [bottom_contour], False, (255, 0, 0), 2)
+    cv2.polylines(bgr_img, [top_contour], False, (0, 0, 255), 2)
+    cv2.imshow('Filled Contour', bgr_img)
+
+
+def extend_contour(contour, left_bound, right_bound, frame_width):
+    left_full, right_full, left_to_right = extend_condition(contour, left_bound, right_bound)
+
+    if left_to_right:
+        begin_pt = contour[0, :]
+        end_pt = contour[-1, :]
+    else:
+        begin_pt = contour[-1, :]
+        end_pt = contour[0, :]
+
+    # These two if blocks determines the filling condition
+    if left_full:
+        left_reach_val = 0
+    else:
+        x_val = left_bound[[0, -1], 0]
+        y_val = left_bound[[0, -1], 1]
+        left_bound_eqn = np.poly1d(np.polyfit(x_val, y_val, 1))
+        left_end_eqn = np.poly1d([0, begin_pt[1]])
+        a, b = list(left_bound_eqn)
+        d = list(left_end_eqn)[0]
+        c=0
+        left_reach_val = (d-b)/(a-c)
+        # left_reach_val = left_bound[left_match_index, 0]
+
+    if right_full:
+        right_reach_val = frame_width - 1
+    else:
+        x_val = right_bound[[0, -1], 0]
+        y_val = right_bound[[0, -1], 1]
+        right_bound_eqn = np.poly1d(np.polyfit(x_val, y_val, 1))
+        right_end_eqn = np.poly1d([0, end_pt[1]])
+        a, b = list(right_bound_eqn)
+        d = list(right_end_eqn)[0]
+        c = 0
+        right_reach_val = (d - b) / (a - c)
+
+    x = contour[[0, -1], 0]
+    y = contour[[0, -1], 1]
+    linear_approx = np.poly1d(np.polyfit(x, y, 1))
+    if begin_pt[0] > left_reach_val:
+        x_array = np.arange(int(left_reach_val), begin_pt[0])
+        y_array = linear_approx(x_array)
+        begin_fill = np.transpose(np.vstack((x_array, y_array)))
+        begin_fill = begin_fill.astype(np.int32)
+    else:
+        begin_fill = np.empty([0, 2], dtype=int)
+
+    if end_pt[0] < right_reach_val:
+        x_array = np.arange(end_pt[0], int(right_reach_val))
+        y_array = linear_approx(x_array)
+        end_fill = np.transpose(np.vstack((x_array, y_array)))
+        end_fill = end_fill.astype(np.int32)
+    else:
+        end_fill = np.empty([0, 2], dtype=int)
+
+    if contour[0, 0] < contour[-1, 0]:
+        filled_contour = np.concatenate((begin_fill, contour, end_fill), axis=0)
+    else:
+        begin_fill = np.flipud(begin_fill)
+        end_fill = np.flipud(end_fill)
+        filled_contour = np.concatenate((end_fill, contour, begin_fill), axis=0)
+
+    return filled_contour
+
+
+
+
+
+def extend_condition(contour, left_bound, right_bound):
+    contour_end_1 = contour[0, :]
+    contour_end_2 = contour[-1, :]
+
+    left_bound_y = np.max(left_bound[:, 1])
+    right_bound_y = np.max(right_bound[:, 1])
+
+    left_to_right = False
+    if contour_end_2[0] > contour_end_1[0]:
+        left_to_right = True
+
+    if left_to_right:
+        left_end = contour_end_1
+        right_end = contour_end_2
+    else:
+        left_end = contour_end_2
+        right_end = contour_end_1
+
+    if left_bound_y < left_end[1]:
+        left_full_width = True
+    else:
+        left_full_width = False
+
+    if right_bound_y < right_end[1]:
+        right_full_width = True
+    else:
+        right_full_width = False
+
+    return left_full_width, right_full_width, left_to_right
 
 
 def remove_bumps(contour):
+    # Using angle as the criteria does not seem to be a good solution
     STARTING_INDEX = 10
     starting_point = contour[STARTING_INDEX, :]
     end_point = contour[-STARTING_INDEX, :]
@@ -45,18 +157,25 @@ def remove_bumps(contour):
     del_y = (ending_y - starting_y)
     del_x = (ending_x - starting_x)
 
-    general_angle = np.arctan2(del_y, del_x)
+    general_angle = np.rad2deg(np.arctan2(del_y, del_x))
     angle_array = create_angle_array(contour, STARTING_INDEX)
-    import matplotlib.pyplot as plt
-    plt.plot(angle_array)
-    plt.show()
-    print()
+
+    average_angle = np.average(angle_array)
+    angle_stdv = np.std(angle_array)
+
+    large_deviations = np.where(angle_array - average_angle > angle_stdv)[0]
+    large_deviations = large_deviations + 2 * DEBUMP_BLOCK_SIZE
+    # import matplotlib.pyplot as plt
+    # plt.plot(angle_array)
+    # plt.show()
+    # print()
+    return large_deviations
 
 
 def create_angle_array(contour, starting_index):
     toReturn = []
 
-    block_size = 10
+    block_size = DEBUMP_BLOCK_SIZE
     for i in range(len(contour) - 3 * starting_index - 1):
         base_point = contour[starting_index + i + 1]
 
@@ -217,7 +336,7 @@ def get_hypotenuse(corner_component):
         upper_corner_y = np.min(upper_corner_y)
         upper_corner = [upper_corner_x, upper_corner_y]
     else:
-        upper_corner_y = hull[upper_corner_index, 1]
+        upper_corner_y = hull[upper_corner_index[0], 1]
         upper_corner = [upper_corner_x, upper_corner_y]
 
     x = [upper_corner[0], lower_corner[0]]
