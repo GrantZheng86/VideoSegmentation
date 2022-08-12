a88916ce7d226f52e6bf8a0227389495acc323d2
Grant <zhengqiaojie@outlook.com>
2022-08-11 22:25:59 -0600

    Nonlinear image enhancement does work for this application

--- Distance_estimation/Distance_measurement.py
+++ Distance_estimation/Distance_measurement.py
@@ -290,16 +290,6 @@ def find_lumbodorsal_bottom_1(top_portion, figure_name, imshow=False):
     figManager.window.showMaximized()
     plt.show()
 
-def image_enchangement(img):
-
-    kernel_size = 5
-    g_func = lambda scale, x_g, y_g: 1./(1. + (np.power(x_g, 2) + (np.power(y_g, 2)) / (scale ** 2)))
-
-
-    for i in range(5000):
-        x_grad = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=kernel_size)
-        y_grad = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=kernel_size)
-        g_val = g_func(2, x_grad, y_grad)
 
 
 
